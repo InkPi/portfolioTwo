27d939ce608bf07b584dbe5e38731ae8b3ca01fd
lindaZ <lz28330@gmail.com>
2020-04-21 15:36:22 -0400

    dynamically style portfolio pg (continuing)

--- src/components/PortfolioBody.vue
+++ src/components/PortfolioBody.vue
@@ -1,21 +1,17 @@
 <template>
     <div id="portfolioBody">
         <p>Portfolio</p>
-         <img src="../../imgfolder/portribbon.png">
+         <img src="../assets/portribbon.png">
     <br>
     <br>
       <div class="image-popup-no-margins">
-        <a v-bind:href="link1" rel="shadowbox" title="My Portfolio Site Design"><div id="port1"></div></a>
-        <a href="../../imgfolder/increasedtemp.jpg" rel="shadowbox" title="Temperature Effect Poster [large]"><div id="port2"></div></a>
-        <a href="../../imgfolder/typo2webfinal2.pdf" rel="shadowbox" title="Wowhuh site"><div id="port3"></div></a>
-        <a href="../../imgfolder/bud.pdf" rel="shadowbox" title="Bud Product"><div id="port4" ></div></a>
-        <a href="../../imgfolder/WalkingSkyscrapers.jpg" rel="shadowbox" title="Walking Skyscrapers Tour Poster"><div id="port5"></div></a>
-        <a href="../../imgfolder/CBI2.jpg" rel="shadowbox" title="Alliance Computing Solutions website"><div id="port6"></div></a>
-        <a href="../../imgfolder/dragon.jpg" rel="shadowbox" title="Dragon Summoned by Candle Flames Poster"><div id="port7"></div></a>
-        <a href="../../imgfolder/SakuraMatsuri.jpg" rel="shadowbox" title="Sakura Matsuri Poster"><div id="port8"></div></a>
-        <a href="../../imgfolder/lionhungryB.pdf" rel="shadowbox" title="Lion's Hungry Appetite"><div id="port9"></div></a>
-        <a href="../../imgfolder/typo2maggridsb.pdf" rel="shadowbox" title="Escape Velocity Magazine Article"><div id="port10"></div></a>
-        <a href="../../imgfolder/acs.pdf" rel="shadowbox" title="Alliance Computing Solutions website"><div id="port11"></div></a>
+        <!-- <a :href="link"><div class="port1"></div></a> -->
+        
+        <a v-for="link in links" :key="link.linkId" :href="link.linkHref">
+            <!-- <div :style="link.styleCss"></div> -->
+          <div :class="'port' + link.linkId"></div>
+        </a>
+
       </div>
       
     </div>
@@ -26,22 +22,140 @@ export default {
     name: "portfolioBody",
     data() {
       return {
-        link1: "../../imgfolder/WebsitePortpres copy.pdf"
+        link: "../assets/WebsitePortprescopy.pdf",
+        links: [
+          {
+            linkId: 1,
+            linkHref: "../assets/WebsitePortprescopy.pdf",
+            styleCss: {
+              width: '320px',
+              height: '300px', 
+              float: 'left', 
+              background: "url('~@/../assets/960_grid_16_col3d.jpg)"
+            }
+          },
+          {
+            linkId: 2,
+            linkHref: "../assets/increasedtemp.jpg",
+            styleCss: {
+              width: '320px', 
+              height: '300px', 
+              float: 'left', 
+              background: "url('../assets/seniorinformationc_icon.jpg')"
+            }
+          },
+          {
+            linkId: 3,
+            linkHref: "../assets/typo2webfinal2.pdf",
+            styleCss: {
+              width: '320px', 
+              height: '600px', 
+              float: 'left', 
+              background: "url('../assets/wowhuhsite.jpg')"
+            }
+          },
+          {
+            linkId: 4,
+            linkHref: "../assets/bud.pdf",
+            styleCss: {
+              width: '320px',
+              height: '300px',
+              float: 'left',
+              background: "url('../assets/SeniorPort1_icon.jpg')"
+            }
+          },
+          {
+            linkId: 5,
+            linkHref: "../assets/WalkingSkyscrapers.jpg",
+            styleCss: {
+              width: '320px', 
+              height: '300px', 
+              float: 'left', 
+              backgroundImage: "url('../assets/typo9_icon.jpg')"
+            } 
+          },
+          {
+            linkId: 6,
+            linkHref: "../assets/CBI2.jpg",
+            styleCss: {
+              width: '640px', 
+              height: '300px', 
+              float: 'left', 
+              background: "url('../assets/CBI_justpic.jpg')"
+            }  
+          },
+          {
+            linkId: 7,
+            linkHref: "../assets/dragon.jpg",
+            styleCss: {
+              width: '320px', 
+              height: '300px', 
+              float: 'left', 
+              background: "url('../assets/SeniorPortDragon_icon.jpg')"
+            } 
+          },
+          {
+            linkId: 8,
+            linkHref: "../assets/SakuraMatsuri.jpg",
+            styleCss: {
+              width: '320px',
+              height: '300px',
+              float: 'left',
+              background: "url('../assets/SeniorPortsakura_icon.jpg')"
+            } 
+          },
+          {
+            linkId: 9,
+            linkHref: "../assets/lionhungryB.pdf",
+            styleCss: {
+              width: '640px', 
+              height: '300px', 
+              float: 'left',
+              background: "url('../assets/27.jpg')"
+            } 
+          },
+          {
+            linkId: 10,
+            linkHref: "../assets/typo2maggridsb.pdf",
+            styleCss: {
+              width: '640px', 
+              height: '300px', 
+              float: 'left',
+              background: "url('../assets/typo2maggrids_icon.jpg')"
+            } 
+          },
+          {
+            linkId: 11,
+            linkHref: "../assets/acs.pdf",
+            styleCss: {
+              width: '320px', 
+              height: '300px', 
+              float: 'left', 
+              background: "url('../assets/acspreporiginal2icon.jpg')"
+            } 
+          }
+        ]
       }
     }
 }
 </script>
 
 <style scoped>
-  #port1 {width: 320px; height: 300px; float:left; background: url('../../imgfolder/960_grid_16_col3d.jpg');}
-  #port2 {width: 320px; height: 300px; float:left; background: url('../../imgfolder/seniorinformationc_icon.jpg');}
-  #port3 {width: 320px; height: 600px; float:right; background: url('../../imgfolder/wowhuhsite.jpg');}
-  #port4 {width: 320px; height: 300px; float:left; background: url('../../imgfolder/SeniorPort1_icon.jpg');}
-  #port5 {width: 320px; height: 300px; float:left; background: url('../../imgfolder/typo9_icon.jpg');}
-  #port6 {width: 640px; height: 300px; float:left; background: url('../../imgfolder/CBI_justpic.jpg');}
-  #port7 {width: 320px; height: 300px; float:left; background: url('../../imgfolder/SeniorPortDragon_icon.jpg');}
-  #port8 {width: 320px; height: 300px; float:left; background: url('../../imgfolder/SeniorPortsakura_icon.jpg');}
-  #port9 {width: 640px; height: 300px; float:left; background: url('../../imgfolder/27.jpg');}
-  #port10 {width: 640px; height: 300px; float:left; background: url('../../imgfolder/typo2maggrids_icon.jpg');}
-  #port11 {width: 320px; height: 300px; float:left; background: url('../../imgfolder/acspreporiginal2icon.jpg');}
+  #portfolioBody {
+    width: 960px;
+    margin: 0px auto;
+  }
+
+  .port1 {width: 320px; height: 300px; float:left; background: url('../assets/960_grid_16_col3d.jpg');}
+  .port2 {width: 320px; height: 300px; float:left; background: url('../assets/seniorinformationc_icon.jpg');}
+  .port3 {width: 320px; height: 600px; float:right; background: url('../assets/wowhuhsite.jpg');}
+  .port4 {width: 320px; height: 300px; float:left; background: url('../assets/SeniorPort1_icon.jpg');}
+  .port5 {width: 320px; height: 300px; float:left; background: url('../assets/typo9_icon.jpg');}
+  .port6 {width: 640px; height: 300px; float:left; background: url('../assets/CBI_justpic.jpg');}
+  .port7 {width: 320px; height: 300px; float:left; background: url('../assets/SeniorPortDragon_icon.jpg');}
+  .port8 {width: 320px; height: 300px; float:left; background: url('../assets/SeniorPortsakura_icon.jpg');}
+  .port9 {width: 640px; height: 300px; float:left; background: url('../assets/27.jpg');}
+  .port10 {width: 640px; height: 300px; float:left; background: url('../assets/typo2maggrids_icon.jpg');}
+  .port11 {width: 320px; height: 300px; float:left; background: url('../assets/acspreporiginal2icon.jpg');}
+
 </style>
