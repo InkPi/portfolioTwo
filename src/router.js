7aa2835450dd905de35a60b11b7ab790e8720500
Jose Bravo <mjabravo221@gmail.com>
2020-05-10 14:16:01 -0700

    router view in app, router changes

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
 
-import Home from "./views/Home.vue";
+import Home from "./views/HomePage.vue";
 // import Home from "./views/HomePage.vue";
 import Portfolio from "./views/PortfolioBody.vue";
 import AboutMe from "./views/AboutMe.vue";
